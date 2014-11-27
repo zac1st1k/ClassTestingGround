582c29b130761b4925cd451f58d348935436264a
zac1st1k <zacfirst@gmail.com>
2014-11-27 19:12:37 +1100

    Initial Commit

---
+++ ClassTestingGroundTests/ClassTestingGroundTests.m
@@ -0,0 +1,34 @@
+//
+//  ClassTestingGroundTests.m
+//  ClassTestingGroundTests
+//
+//  Created by Zac on 27/11/2014.
+//  Copyright (c) 2014 1st1k. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ClassTestingGroundTests : XCTestCase
+
+@end
+
+@implementation ClassTestingGroundTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
